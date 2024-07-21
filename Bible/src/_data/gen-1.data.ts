bfd0e2ad73ed5fdc4b6580c4f045e0d1a2621678
Greg Marine <gregmarine@hey.com>
2024-07-20 21:40:43 -0400

    Switching to data interface to get scripture at build time

---
+++ Bible/src/_data/gen-1.data.ts
@@ -0,0 +1,26 @@
+import { BibleClient } from '@gracious.tech/fetch-client'
+// Init client
+const client = new BibleClient()
+
+export default {
+  async load(): Promise<string> {
+
+    // Fetch the collection's meta data
+    const collection = await client.fetch_collection()
+
+    // Get what translations are available
+    // const translations = collection.get_translations()
+
+    // Get the id of the first translation available
+    const translation_id = 'eng_bsb' // translations[0].id
+
+    // Get what books are available for the translation
+    // (may be whole Bible or may only be e.g. NT)
+    const books = collection.get_books(translation_id)
+
+    // Fetch the contents of the first book
+    const book = await collection.fetch_book(translation_id, books[0].id)
+
+    return book.get_chapter(1)
+  }
+}
